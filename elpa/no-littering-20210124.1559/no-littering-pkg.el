5d551d1c11ac67bfbefacb58c38d4dc85132080d
ssoukssouk <souk@cyberdude.com>
2021-03-16 19:04:23 +0100

    version ou je test les premier packages

--- elpa/no-littering-20210124.1559/no-littering-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from c:/Users/Mehdi/.emacs.d/elpa/no-littering-20210124.1559/no-littering.el  -*- no-byte-compile: t -*-
-(define-package "no-littering" "20210124.1559" "help keeping ~/.emacs.d clean" '((cl-lib "0.5")) :commit "57357e15643158b4e0d9b3b4f70a82f5fc73178a" :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/emacscollective/no-littering")
