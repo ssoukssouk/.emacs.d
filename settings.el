6c3ba2e3393ef053e9092f2129e14523364b0582
Mehdi Maison <mehdimebkhout@gmail.com>
2021-03-25 22:31:11 +0100

    add of smooth scrolling

--- settings.el
+++ settings.el
@@ -32,7 +32,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
- (setq auto-save-default nil)
+ ;; (setq auto-save-default nil)
 
 ;; Change the user-emacs-directory to keep unwanted things out of ~/.emacs.d
 (setq user-emacs-directory (expand-file-name "~/.cache/emacs/")
