985b38f19c0eb9a389b608adfb33236de24d3791
Mehdi Maison <mehdimebkhout@gmail.com>
2022-04-12 21:14:00 +0200

    ajout de €

--- bin/emacs/var/nsm-settings.el
+++ bin/emacs/var/nsm-settings.el
@@ -1,3 +1,4 @@
 (
+ (:id "sha1:24c7a1e6369fef1e50de1deb1a9e5bbc68f80c8b" :fingerprints ("sha1:25:7c:be:c0:7f:a7:3b:86:ae:e9:04:c4:a8:ca:77:33:2f:91:40:be") :host "orgmode.org:443" :conditions (:expired :invalid :verify-cert))
  (:id "sha1:85b31c268009209a8d3c5387033b219264f7e62b" :fingerprint "sha1:b5:b9:09:73:35:bc:48:5a:1f:f1:37:33:e5:c0:89:7e:e2:5f:6a:6c" :host "melpa.org:443" :conditions (:expired :invalid))
 )
